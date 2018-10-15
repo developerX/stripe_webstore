32b0dc0a2eee86dd7239909782cc431e92a56b8f
Mansoor Bahramand <mansoor.bahramand@gMB96db97.local>
2018-10-15 16:25:20 -0700

    added a new route of success

--- app.rb
+++ app.rb
@@ -3,3 +3,8 @@ require 'sinatra'
 get '/' do
   "This is the home page."
 end
+
+get '/success' do
+  p params
+  "Your payment has processed  #{params[:cardNumber] ? "with #{params[:cardNumber]}" : ""} successfully"
+end
