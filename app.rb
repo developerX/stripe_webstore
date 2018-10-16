cfaac4d4092471cb64c5946958def3655e93c860
Mansoor Bahramand <mansoor.bahramand@gMB96db97.local>
2018-10-15 18:10:37 -0700

    added database, erb view, and stylesheet. Also passed vars to the view

--- app.rb
+++ app.rb
@@ -1,7 +1,21 @@
 require 'sinatra'
+require 'sqlite3'
 
 get '/' do
-  "This is the home page."
+  # connect to the database
+  db = SQLite3::Database.open('store.db')
+
+  # configure results to be returned as as an array of hashes instead of nested arrays
+  db.results_as_hash = true
+
+  # query the products table and print the result
+  puts "Database query results:"
+  @products = db.execute("SELECT description, price FROM products;")
+  p @products
+  # close database connection
+  db.close
+  @crazyTest = "This is a crazy test"
+  erb :home
 end
 
 get '/success' do
